0d97b432f525b31d563a4dd71554c1395d627e7c
Otavio Lhamas <otavio.m.lhamas@gmail.com>
2026-05-17 14:11:12 -0300

    feat: add github user to bootstrap init command

--- bootstrap.sh
+++ bootstrap.sh
@@ -33,7 +33,7 @@ fi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
-set -- init --apply --source="${script_dir}"
+set -- init OtavioLhamas --apply --source="${script_dir}/chezmoi"
 
 echo "Running 'chezmoi $*'" >&2
 # exec: replace current process with chezmoi
